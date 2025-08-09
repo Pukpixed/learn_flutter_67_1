4f35ca63c94e15a7f51463262d8d7a98b960b4e0
Pukpixed <rmenavijitnavee@gmail.com>
2025-08-09 15:02:46 +0700

    Step 3: Stateless

--- lib/main.dart
+++ lib/main.dart
@@ -4,73 +4,26 @@ void main() {
   //runApp(const MyApp());
   //const app = MaterialApp(title:"หัวข้อ", home:Text("Hello World"));
   //runApp(app);
-  runApp(MaterialApp(title:"หัวข้อ",home: Scaffold(appBar: AppBar(title: Text("My App"),
-  backgroundColor: Colors.indigoAccent
-  centerTitle: true,
-  ), 
-  body: Text("Hello Flutter"),
-  )
-  ),
+  runApp(
+    MaterialApp(
+      title: "หัวข้อ",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+          backgroundColor: Colors.indigoAccent,
+          centerTitle: true,
+        ),
+        body: Home(),
+      ),
+    ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Text("Hello flutter! Step 2");
   }
 }
