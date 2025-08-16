ca50c1f32aa151ccce5ee5c62bbf33a636fddcb9
Pukpixed <rmenavijitnavee@gmail.com>
2025-08-16 14:54:06 +0700

    Step 10: make a model for store data

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_67_1/model/person.dart';
 
 class Item extends StatefulWidget {
   const Item({super.key});
@@ -8,12 +9,10 @@ class Item extends StatefulWidget {
 }
 
 class _ItemState extends State<Item> {
-  List data = ["สมชาย", "สมหญิง", "สมศรี", "สมปอง", "สมจิตร"];
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: data.length,
+      itemCount: personList.length,
       itemBuilder: (context, index) {
         return Container(
           decoration: BoxDecoration(
@@ -22,9 +21,22 @@ class _ItemState extends State<Item> {
           ),
           margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
           padding: EdgeInsets.all(40),
-          child: Text(
-            data[index],
-            style: TextStyle(fontSize: 20, color: Colors.white),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                personList[index].name,
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              Text(
+                "${personList[index].age} ปี",
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              Text(
+                personList[index].job,
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ],
           ),
         );
       },
