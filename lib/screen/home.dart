d54e072ae0157dede95540b5699dffa590e016cb
Pukpixed <rmenavijitnavee@gmail.com>
2025-08-16 11:52:04 +0700

    Step 7: Button widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class home extends StatelessWidget {
+  const home({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +10,7 @@ class Home extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextButton(
-            style: TextButton.styleFrom(
-              foregroundColor: const Color.fromARGB(255, 255, 33, 70),
-            ),
+            style: TextButton.styleFrom(foregroundColor: Colors.red),
             onPressed: () {
               print("Button Pressed");
             },
@@ -22,11 +20,24 @@ class Home extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20),
+          FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.pinkAccent,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              print("Filled Button Pressed");
+            },
+            child: const Text(
+              "Click Me",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 20),
           OutlinedButton(
             style: OutlinedButton.styleFrom(
-              foregroundColor: const Color.fromARGB(255, 255, 33, 70),
-              side: const BorderSide(
-                  color: Color.fromARGB(255, 255, 255, 255), width: 2),
+              backgroundColor: Colors.pinkAccent,
+              side: const BorderSide(color: Colors.pinkAccent, width: 2),
             ),
             onPressed: () {
               print("Outlined Button Pressed");
@@ -39,11 +50,11 @@ class Home extends StatelessWidget {
           const SizedBox(height: 20),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
-              backgroundColor: const Color.fromARGB(255, 255, 69, 205),
-              foregroundColor: Colors.white,
+              backgroundColor: Colors.pinkAccent,
+              side: const BorderSide(color: Colors.white, width: 2),
             ),
             onPressed: () {
-              print("Elevated Button Pressed");
+              print("Outlined Button Pressed");
             },
             child: const Text(
               "Click Me",
@@ -53,5 +64,5 @@ class Home extends StatelessWidget {
         ],
       ),
     );
-  } // ← ปิด method build
-} // ← ปิด class Home
+  }
+}
